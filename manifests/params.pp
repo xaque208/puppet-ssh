966c2f214df32c32ee1133032a3850831ea08347
Adrien Thebo <adrien@puppetlabs.com>
2012-10-15 11:46:20 -0700

    Clean up ssh params, add ssh_dir and known_hosts

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,45 +1,37 @@
 class ssh::params {
 
+  $ssh_dir           = '/etc/ssh'
+  $sshd_config       = "${ssh_dir}/sshd_config"
+  $ssh_config        = "${ssh_dir}/ssh_config"
+  $known_hosts       = "${ssh_dir}/ssh_known_hosts"
+
   case $operatingsystem {
     'centos', 'redhat', 'fedora': {
-      $client_package    = 'openssh-clients'
-      $server_package    = 'openssh-server'
-      $ssh_service       = 'sshd'
-      $sshd_config       = '/etc/ssh/sshd_config'
-      $ssh_config       = '/etc/ssh/ssh_config'
+      $client_package = 'openssh-clients'
+      $server_package = 'openssh-server'
+      $ssh_service    = 'sshd'
     }
     'sles': {
-      $client_package    = 'openssh'
-      $server_package    = 'openssh'
-      $ssh_service       = 'sshd'
-      $sshd_config       = '/etc/ssh/sshd_config'
-      $ssh_config       = '/etc/ssh/ssh_config'
+      $client_package = 'openssh'
+      $server_package = 'openssh'
+      $ssh_service    = 'sshd'
     }
     'ubuntu', 'debian': {
-      $client_package    = 'openssh-client'
-      $server_package    = 'openssh-server'
-      $ssh_service       = 'ssh'
-      $sshd_config       = '/etc/ssh/sshd_config'
-      $ssh_config       = '/etc/ssh/ssh_config'
+      $client_package = 'openssh-client'
+      $server_package = 'openssh-server'
+      $ssh_service    = 'ssh'
     }
     'darwin': {
       $ssh_service = 'com.openssh.sshd'
-      $ssh_config  = '/etc/ssh_config'
-      $sshd_config = '/etc/sshd_config'
     }
     'freebsd': {
       $ssh_service = 'sshd'
-      $ssh_config  = '/etc/ssh_config'
-      $sshd_config = '/etc/sshd_config'
     }
     'solaris','sunos': {
-      $client_package    = 'network/ssh'
-      $server_package    = 'network/ssh'
-      $ssh_service       = 'ssh'
-      $ssh_config        = '/etc/ssh_config'
-      $sshd_config       = '/etc/sshd_config'
+      $client_package = 'network/ssh'
+      $server_package = 'network/ssh'
+      $ssh_service    = 'ssh'
     }
-
     default: {
       fail("module ssh does not support operatingsystem $operatingsystem")
     }
