e0512510801e52617d8711d831542a80a97c8e42
Zach Leslie <zleslie@womply.com>
2016-06-06 14:34:19 -0700

    Improve support for space separated options
    This work adds a known list of options who's values are space separated
    to allow the template to check if Array values received should joined by
    commas or spaces.

--- spec/classes/ssh_server_config_spec.rb
+++ spec/classes/ssh_server_config_spec.rb
@@ -12,6 +12,7 @@ describe 'ssh::server::config' do
   it { should contain_class('ssh::server::config') }
 
   valid_keywords = [
+    'AcceptEnv',
     'AddressFamily',
     'AllowAgentForwarding',
     'AllowTcpForwarding',
@@ -99,6 +100,31 @@ describe 'ssh::server::config' do
     let(:params) { {:ciphers => ['chacha20-poly1305@openssh.com', 'aes256-ctr']} }
 
     it { should contain_concat__fragment('sshd_config').with_content(/^Ciphers chacha20-poly1305@openssh.com,aes256-ctr$/) }
-
   end
+
+  context 'with denyusers => []' do
+    let(:params) { {:acceptenv => ['AWS_*', 'TERM']} }
+
+    it { should contain_concat__fragment('sshd_config').with_content(/^AcceptEnv AWS_\* TERM$/) }
+  end
+
+  context 'with denyusers => []' do
+    let(:params) { {:denyusers => ['test1', 'test2']} }
+
+    it { should contain_concat__fragment('sshd_config').with_content(/^DenyUsers test1 test2$/) }
+  end
+
+  context 'with denygroups => []' do
+    let(:params) { {:denygroups => ['test1', 'test2']} }
+
+    it { should contain_concat__fragment('sshd_config').with_content(/^DenyGroups test1 test2$/) }
+  end
+
+  context 'with authorizedkeysfile => []' do
+    let(:params) { {:authorizedkeysfile => ['/path/one', '/path/two']} }
+
+    it { should contain_concat__fragment('sshd_config').with_content(/^AuthorizedKeysFile \/path\/one \/path\/two$/) }
+  end
+
+
 end
