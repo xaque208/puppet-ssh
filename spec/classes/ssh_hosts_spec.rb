1b0dc3a140939bb88913a9056212cdaade5ba17c
Zach Leslie <xaque208@gmail.com>
2016-07-13 08:24:38 -0700

    Fix tests for host keys
    This work compensates for the fact that not all platforms have all host
    key types.

--- spec/classes/ssh_hosts_spec.rb
+++ spec/classes/ssh_hosts_spec.rb
@@ -2,13 +2,24 @@ require 'spec_helper'
 
 describe 'ssh::hosts' do
   on_supported_os.each do |os,facts|
-    let(:facts) { facts }
-    let(:params) { {:host_aliases => ['foo']} }
+    context "on #{os}" do
+      let(:facts) { facts }
+      let(:params) { {:host_aliases => ['foo']} }
 
-    it { should contain_class('ssh::hosts') }
+      it { should contain_class('ssh::hosts') }
 
-    it { expect(exported_resources).to contain_sshkey('sshecdsakey-foo') }
-    it { expect(exported_resources).to contain_sshkey('sshed25519key-foo') }
-    it { expect(exported_resources).to contain_sshkey('sshdsakey-foo') }
+      if facts.keys.include? :sshecdsakey
+        it { expect(exported_resources).to contain_sshkey('sshecdsakey-foo') }
+      end
+
+      if facts.keys.include? :sshdsakey
+        it { expect(exported_resources).to contain_sshkey('sshdsakey-foo') }
+      end
+
+      if facts.keys.include? :sshed25519key
+        it { expect(exported_resources).to contain_sshkey('sshed25519key-foo') }
+      end
+
+    end
   end
 end
